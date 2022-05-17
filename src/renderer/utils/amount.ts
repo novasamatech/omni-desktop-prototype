e301f7d0ab93bacac584e197f8d13e8f8cad009c
asmadek <alma2610@ya.ru>
2022-05-17 17:02:31 +0500

    chore: fix comments

--- src/renderer/utils/amount.ts
+++ src/renderer/utils/amount.ts
@@ -2,23 +2,21 @@
 import { BN, BN_TEN } from '@polkadot/util';
 
 export const formatAmount = (amount: string, precision: number): string => {
-  console.log(precision);
   // eslint-disable-next-line @typescript-eslint/prefer-regexp-exec
   const isDecimalValue = amount.match(/^(\d+)\.(\d+)$/);
   const bnPrecision = new BN(precision);
-  let result;
-
   if (isDecimalValue) {
     const div = new BN(amount.replace(/\.\d*$/, ''));
-    const modString = amount.replace(/^\d+\./, '').substr(0, precision);
+    const modString = amount.replace(/^\d+\./, '').slice(0, precision);
     const mod = new BN(modString);
 
-    result = div
+    return div
       .mul(BN_TEN.pow(bnPrecision))
-      .add(mod.mul(BN_TEN.pow(new BN(precision - modString.length))));
-  } else {
-    result = new BN(amount.replace(/[^\d]/g, '')).mul(BN_TEN.pow(bnPrecision));
+      .add(mod.mul(BN_TEN.pow(new BN(precision - modString.length))))
+      .toString();
   }
-  console.log(result.toString());
-  return result.toString();
+
+  return new BN(amount.replace(/[^\d]/g, ''))
+    .mul(BN_TEN.pow(bnPrecision))
+    .toString();
 };
