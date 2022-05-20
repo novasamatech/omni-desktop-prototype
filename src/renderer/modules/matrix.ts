1bcea93b7c00ba7f78428a3a3633276c7ae1dd55
AgAgent050 <yaroslav@augmented.finance>
2022-05-20 09:55:27 +0300

    chore: remove duplicates in invites

--- src/renderer/modules/matrix.ts
+++ src/renderer/modules/matrix.ts
@@ -18,6 +18,7 @@ import {
   Visibility,
 } from 'matrix-js-sdk';
 import { SyncState } from 'matrix-js-sdk/lib/sync';
+import { uniq } from 'lodash';
 import { HexString } from '../../common/types';
 import { AuthState, OmniDexie } from '../db/db';
 
@@ -301,13 +302,19 @@ class Matrix implements SecureMessenger {
     roomId: string,
     signatories: Signatories,
   ): Promise<void> {
-    const inviteRequests = signatories
-      .filter((signatory) => !signatory.isInviter)
-      .reduce((acc, signatory) => {
-        acc.push(this.matrixClient.invite(roomId, signatory.matrixAddress));
+    const inviterAddress = signatories.find((s) => s.isInviter)?.matrixAddress;
 
-        return acc;
-      }, [] as Promise<unknown>[]);
+    const noDuplicates = uniq(
+      signatories
+        .filter((s) => !s.isInviter && s.matrixAddress !== inviterAddress)
+        .map((s) => s.matrixAddress),
+    );
+
+    const inviteRequests = noDuplicates.reduce((acc, matrixAddress) => {
+      acc.push(this.matrixClient.invite(roomId, matrixAddress));
+
+      return acc;
+    }, [] as Promise<unknown>[]);
 
     await Promise.all(inviteRequests);
   }
